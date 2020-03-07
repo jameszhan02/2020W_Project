9dc7546220b2c29d43554bc7f388e1b2c3f98d32
詹晟 <sillyknight@zhanshengdeMBP.fc.ca>
2020-03-07 15:07:52 -0500

    task added

---
+++ project_SSG/project_SSG/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project_SSG
+{
+    class Task
+    {
+        string content { get; set; }
+        string id { get; set; }
+        string parent { get; set; } = null;
+    }
+}
